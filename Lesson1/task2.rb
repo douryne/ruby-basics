af3d4947359dbb8bb0bb9326db3c49b9fbe2be77
douryne <douryne.1st@gmail.com>
2024-06-04 09:37:39 +0500

    lesson 1 task 2

---
+++ Lesson1/task2.rb
@@ -0,0 +1,11 @@
+puts "Enter the height of the triangle"
+
+height = gets.to_f
+
+puts "Enter the base of the triangle"
+
+base = gets.to_f
+
+area = height * base * 1/2
+
+puts "The area of the triangle is: #{area}"
