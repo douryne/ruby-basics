458a5cf94a9a57fb4c4e5762d3bbd13882614dd7
douryne <douryne.1st@gmail.com>
2024-06-12 19:06:07 +0500

    finish task5

--- Lesson2/task5.rb
+++ Lesson2/task5.rb
@@ -1,14 +1,17 @@
+@months = { "january" => 31, "february" => 28, "march" => 31, "april" => 30, "may" => 31, "june" => 30, "july" => 31, "august" => 31, "september" => 30, "october" => 31, "november" => 30, "december" => 31 }
+
 def get_date_counter(day, month, year)
-  months["february"] = 29 if leap_year?(year)
-  days_count = months.values[0...(month - 1)].sum
-  return days_count + (days_count - day)
+  @months["february"] = 29 if leap_year?(year)
+  puts leap_year?(year)
+  days_count = @months.values[0...(month - 1)].sum
+  return month == 1 ? day : days_count + day
 end
 
 def leap_year?(year)
-  year % 4 == 0 && year % 100 == 0 && year % 400 == 0
+  return true if year % 4 == 0
+  return false if year % 100 != 0
+  year % 400 == 0
 end
-
-months = { "january" => 31, "february" => 28, "march" => 31, "april" => 30, "may" => 31, "june" => 30, "july" => 31, "august" => 31, "september" => 30, "october" => 31, "november" => 30, "december" => 31 }
 
 puts "Enter the days value"
 day = gets.to_i
